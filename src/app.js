1fae345b7da752656c3d9091023ce10e8a997599
victorgabrielms <vgabriel.vg84@gmail.com>
2020-08-17 21:01:58 -0300

    google api configuration, and test request

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const { errors } = require("celebrate");
 const routes = require("./routes");
 
 const app = express();
@@ -8,6 +7,5 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.use(errors());
 
 module.exports = app;
